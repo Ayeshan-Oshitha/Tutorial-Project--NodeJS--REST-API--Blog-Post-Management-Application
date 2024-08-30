d7af46eda3df385d807bce51559a51fc78cb5498
Ayeshan-Oshitha <149539603+Ayeshan-Oshitha@users.noreply.github.com>
2024-08-30 17:55:27 +0530

    367. REST APIs, Clients & CORS Errors

--- BackEnd-Server/app.js
+++ BackEnd-Server/app.js
@@ -1,12 +1,20 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const feedRoutes = require('./routes/feed');
-const bodyParser = require('body-parser');
+
 
 const app = express();
 
 //app.use(bodyParser.urlencoded());  //x-www-form-urlencoded <form>
 app.use(bodyParser.json());  // application/json
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Any-Origin', '*');
+    res.setHeader('Access-Control-Any-Methods','GET, POST, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Any-Headers', 'Content-Type, Authorization');
+    next();
+})
 
 app.use('/feed' , feedRoutes)  // Can write as app.use(feedRoutes)
 
