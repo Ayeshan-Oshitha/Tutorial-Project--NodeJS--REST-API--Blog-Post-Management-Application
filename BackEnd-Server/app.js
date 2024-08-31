44048903bb2963721fbfc9543fe399c7a887b79d
Ayeshan-Oshitha <149539603+Ayeshan-Oshitha@users.noreply.github.com>
2024-08-31 09:37:41 +0530

    380. Static Images & Error Handling ( Custom error Handling )

--- BackEnd-Server/app.js
+++ BackEnd-Server/app.js
@@ -1,3 +1,5 @@
+const path = require('path');
+
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
@@ -9,6 +11,7 @@ const app = express();
 
 //app.use(bodyParser.urlencoded());  //x-www-form-urlencoded <form>
 app.use(bodyParser.json());  // application/json
+app.use('/images', express.static(path.join(__dirname,'images')))
 
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');  // Allow all origins
@@ -18,6 +21,13 @@ app.use((req, res, next) => {
 })
 
 app.use('/feed' , feedRoutes)  // Can write as app.use(feedRoutes)
+
+app.use((error, req, res, next) => {
+    console.log(error);
+    const status = error.statusCode || 500;
+    const message = error.message;
+    res.status(status).json({message : message})
+})
 
 mongoose
   .connect(
