502d7fc4f95c0fc33ab390aee73781d3ae8a7ea3
Ayeshan-Oshitha <149539603+Ayeshan-Oshitha@users.noreply.github.com>
2024-08-31 09:24:24 +0530

    379. Storing Data(Posts) in the Database

--- BackEnd-Server/controller/feed.js
+++ BackEnd-Server/controller/feed.js
@@ -1,5 +1,7 @@
 const { validationResult } = require('express-validator')
 
+const Post = require('../models/post');
+
 exports.getPosts = (req, res, next) => {
   res.status(200).json({
     posts: [
@@ -28,12 +30,23 @@ exports.createPosts = (req, res, next) => {
   }
   const title = req.body.title;
   const content = req.body.content;
-  //create posts in db
-  res.status(201).json({
-    message: "Post created successfully",
-    post: { _id: new Date().toISOString(), title: title, content: content,
-        creator:{name:'Max'},
-        createdAt: new Date()
-     },
+  const post = new Post({
+    title: title, 
+    content: content,
+    imageUrl: 'images/duck.png',
+    creator:{name:'Max'}
   });
+  post
+    .save()
+    .then(result => {
+      console.log(result);
+      res.status(201).json({
+        message: "Post created successfully",
+        post: result
+      });
+    })
+    .catch(err => {
+      console.log(err);
+    });
+  
 };
